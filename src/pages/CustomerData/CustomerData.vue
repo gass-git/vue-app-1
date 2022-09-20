9a716dc60541894a2e0a498c255595ef4481ac8d
gabriel salinas <g.szada@gmail.com>
2022-09-20 16:14:10 +0200

    Made changes to the table in CustomerData component

--- src/pages/CustomerData/CustomerData.vue
+++ src/pages/CustomerData/CustomerData.vue
@@ -3,14 +3,16 @@
         <tr>
             <th>first name</th>
             <th>last name</th>
+            <th>sex</th>
             <th>age</th>
-            <th>height</th>
-            <th>weight</th>
+            <th>height (m)</th>
+            <th>weight (kg)</th>
             <th>fat percentage</th>
         </tr>
         <tr v-for="customer in customers" v-bind:key="customer.id">
             <td>{{ customer.firstName }}</td>
             <td>{{ customer.lastName }}</td>
+            <td>{{ customer.sex }}</td>
             <td>{{ customer.age }}</td>
             <td>{{ customer.height }}</td>
             <td>{{ customer.weight }}</td>
@@ -52,12 +54,13 @@ table{
 }
 tr > * {
     text-align: left;
-    padding:8px 25px 8px 25px;   
+    padding:12px 30px 12px 30px;   
 }
 th{
     text-transform: capitalize; 
 }
 td{
+    color:rgb(95, 95, 95);
     border-top:1px solid #F2F2F2;
 }
 </style>
