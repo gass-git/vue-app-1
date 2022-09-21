f83dd7c8fcd8897c44859afdb0d13aeb0b02aef5
gabriel salinas <g.szada@gmail.com>
2022-09-21 18:33:58 +0200

    ThemeToggle component improved

--- src/components/ThemeToggle.vue
+++ src/components/ThemeToggle.vue
@@ -1,20 +1,36 @@
 <template>
     <div id="container">
-        <div id="square"></div>
+        <div id="square" @click="changeTheme"></div>
     </div>
 </template>
 
-<script>
+<script setup>
+import {ref} from 'vue'
+
+const current = ref('light')
+const position = ref('translate(0px)')
+
+function changeTheme(){
+    if(current.value === 'light'){
+        current.value = 'dark'
+        position.value = 'translate(50px)'
+    }
+    else{
+        current.value = 'light'
+        position.value = 'translate(0px)'
+    }
+}
 
 </script>
+
 
 <style scoped>
     #container{
         width:100px;
         height:50px;
-
         border:1px solid #c9c9c9;
         border-radius:5px;
+        
     }
     #square{
         margin:2px;
@@ -23,5 +39,7 @@
         border-radius:3px;
         background:#41b883;
         cursor:pointer;
+        transition: 300ms;
+        transform:v-bind(position);
     }
 </style>
