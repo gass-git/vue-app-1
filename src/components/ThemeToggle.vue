049e608e8b4e5f66fc1e6c75ded525d34e436000
gabriel salinas <g.szada@gmail.com>
2022-09-22 09:52:38 +0200

    Added sun and moon icons to themeToggle component

--- src/components/ThemeToggle.vue
+++ src/components/ThemeToggle.vue
@@ -1,6 +1,9 @@
 <template>
     <div id="container">
-        <div id="square" @click="changeTheme"></div>
+        <div id="square" @click="changeTheme">
+                <fa v-if="lightThemeIsActive" icon="fa-solid fa-sun" class="white-icon" />
+                <fa v-else icon="fa-solid fa-moon" class="white-icon" />
+        </div>
     </div>
 </template>
 
@@ -9,18 +12,26 @@ import {ref} from 'vue'
 
 const current = ref('light')
 const position = ref('translate(0px)')
+const lightThemeIsActive = ref(true)
 
 function changeTheme(){
     if(current.value === 'light'){
         current.value = 'dark'
         position.value = 'translate(50px)'
+
+        setTimeout(() => {
+            lightThemeIsActive.value = false
+        }, 100)
     }
     else{
         current.value = 'light'
         position.value = 'translate(0px)'
+
+        setTimeout(() => {
+            lightThemeIsActive.value = true
+        }, 100)
     }
 }
-
 </script>
 
 
@@ -28,7 +39,7 @@ function changeTheme(){
     #container{
         width:100px;
         height:50px;
-        border:1px solid #c9c9c9;
+        border:2px solid #d5d5d5;
         border-radius:5px;
         
     }
@@ -36,10 +47,18 @@ function changeTheme(){
         margin:2px;
         aspect-ratio: 1;
         height:46px;
+
+        display:grid;
+        place-items:center;
+
         border-radius:3px;
         background:#41b883;
         cursor:pointer;
         transition: 300ms;
         transform:v-bind(position);
+    }
+    .white-icon{
+        color:#fff;
+        font-size:25px;
     }
 </style>
